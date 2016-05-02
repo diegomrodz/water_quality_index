58729a2764792ce36e35eddf1659d9a02509c672
Diego Rodrigues <diegomrodz@gmail.com>
2016-05-02 05:50:07 +0000

    Erro Indentificado: Burrice Humana

--- test/iqa_test.c
+++ test/iqa_test.c
@@ -19,20 +19,32 @@ int main(void)
 	double a = iqa(t);
 
 	IQA_T t2 = {
-		.O2  = qO2(40),
-		.CF  = qCF(5),
+		.O2  = qO2(120),
+		.CF  = qCF(50),
 		.PH  = qPH(8),
 		.DBO = qDBO(3),
-		.DT  = qDT(0),
+		.DT  = qDT(6),
 		.NT  = qNT(90),
 		.FT  = qFT(1),
 		.TU  = qTU(5),
 		.ST  = qST(50)
 	};
 
-	double b = iqa(t);
+	double b = iqa(t2);
 
 	printf("IQA 1 = %lf\n", a);
+
 	printf("IQA 2 = %lf\n", b);
+
+
+	printf("IQA.O2 = %lf\n", t2.O2);
+	printf("IQA.CF = %lf\n", t2.CF);
+	printf("IQA.PH = %lf\n", t2.PH);
+	printf("IQA.DBO = %lf\n", t2.DBO);
+	printf("IQA.DT = %lf\n", t2.DT);
+	printf("IQA.NT = %lf\n", t2.NT);
+	printf("IQA.FT = %lf\n", t2.FT);
+	printf("IQA.TU = %lf\n", t2.TU);
+	printf("IQA.ST = %lf\n", t2.ST);
 
 }
