0519a7a86dd4b23fb6b546f91ede86e310fd8b0c
Diego Rodrigues <diegomrodz@gmail.com>
2016-05-20 14:51:51 -0400

    Erro com template Glade resolvido! Amem.

--- main.c
+++ main.c
@@ -4,14 +4,13 @@
 #include <gtk/gtk.h>
 #include "iqa/iqa.h"
 
-const char* gTemplatePath = "./layout/iqa_glade_template_v100.glade";
+const char* gTemplatePath = "layout/iqa_glade_template_v100.glade";
 
 GtkBuilder* gBuilder;
-GObject* gWindow;
+GtkWidget* gWindow;
 
 int main(int argc, char* argv[]) 
 {
-    GObject* button;
     GError* err = NULL;
 
     gtk_init(&argc, &argv);
@@ -25,11 +24,12 @@ int main(int argc, char* argv[])
         return 0;
     }
 
-    gWindow = gtk_builder_get_object(gBuilder, "mainapplication");
+    gWindow = GTK_WIDGET(gtk_builder_get_object(gBuilder, "window"));
     g_signal_connect(gWindow, "destroy", G_CALLBACK(gtk_main_quit), NULL);    
 
-    button = gtk_builder_get_object(gBuilder, "quit");
-    g_signal_connect(button, "clicked", G_CALLBACK(gtk_main_quit), NULL);
+    g_object_unref(G_OBJECT(gBuilder));
+
+    gtk_widget_show(gWindow);
 
     gtk_main();
 
